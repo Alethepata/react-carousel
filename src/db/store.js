d8054c34c1df4f3b7601ae80d100201179d05968
Alethepata <alessia.patane@gmail.com>
2024-06-14 14:31:21 +0200

    add store

---
+++ src/db/store.js
@@ -0,0 +1,22 @@
+export const store = [
+    {
+        title: 'Immagine uno',
+        image: 'https://placedog.net/640/480'
+    },
+    {
+        title: 'Immagine due',
+        image: 'https://placedog.net/640/490'
+    },
+    {
+        title: 'Immagine tre',
+        image: 'https://placedog.net/640/450'
+    },
+    {
+        title: 'Immagine quattro',
+        image: 'https://placedog.net/640/470'
+    },
+    {
+        title: 'Immagine cinque',
+        image: 'https://placedog.net/640/482'
+    }
+]
